2ac0b5f499ee88b04d9c773fbfb7c3b61b9bc903
LBeizaIndo <lfbeiza@miuandes.com>
2024-04-24 13:14:17 -0400

    advance validates

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,6 @@ class Post < ApplicationRecord
     
     validates :title, presence: true
     validates :content, presence: true
+    validates :answers_count, numericality: { greater_than_or_equal_to: 0 }
+    validates :likes_count, numericality: { greater_than_or_equal_to: 0 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
     has_many :posts
 
     validates :name, presence: true
-    validates :email, presence: true, uniqueness: true
-    validates :password, presence: true
+    validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :password, presence: true,  length: { minimum: 6 }
 end
